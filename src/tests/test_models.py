d6dfd0f8ae5fb6014b3a1fa840d6730de47bcb8a
DM880 <denys.mauro@gmail.com>
2021-10-21 05:10:36 +0000

    test models creation

---
+++ src/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+
+from store.data.category.models import CategoryItem as Category
+from store.data.item.models import Item
+from store.data.user.models import CustomUser as User
+
+
+class ItemTest(TestCase):
+
+    def create_item(self):
+
+        item_data = {
+            'item_name': "Test Item",
+            'item_description': "This is a test description",
+            'price': 20.20,
+            'reviews': "This a Test review",
+        }
+
+        return Item.objects.create(**item_data)
+
+
+    def test_item_creation(self):
+        item_ex = self.create_item()
+        self.assertTrue(isinstance(item_ex, Item))
+
+
+class UserTest(TestCase):
+
+    def create_user(self):
+
+        user_info = {
+            'first_name': 'New',
+            'last_name': 'User',
+            'username': 'Test',
+            'date_of_birth': '2021-10-21',
+            'email': 'thisisatest@test.com',
+            'password': 'test12345',
+        }
+
+        return User.objects.create(**user_info)
+
+
+    def test_user_create(self):
+        user_ex = self.create_user()
+        self.assertTrue(isinstance(user_ex, User))
