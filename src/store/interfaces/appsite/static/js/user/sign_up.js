d6e8bbdd079f0b4eeba630be7d5135bb95381aad
DM880 <denys.mauro@gmail.com>
2021-10-30 09:27:23 +0000

    password validation added with js for signup form

--- src/store/interfaces/appsite/static/js/user/sign_up.js
+++ src/store/interfaces/appsite/static/js/user/sign_up.js
@@ -1,76 +1,29 @@
-function password_validation(){
+var check_password = function() {
 
-    password1 = document.getElementByID('password1');
-    password2 = document.getElementByID('password2');
+  password1 = document.getElementById('password1');
+  password2 = document.getElementById('password2');
+  button = document.getElementById('submit-btn');
+  message = document.getElementById('message');
 
-
-    if(password1.value.length < 8){
-        password1.InnerHtml = "Password must be more than 8 character long";
-    }
-
-    else if(password1 != password2){
-        password1.innerHTML = "Passwords don't match";
-        password2.innerHTML = "Passwords don't match";
-    }
-
-};
-
-  document.addEventListener("DOMContentLoaded", function() {
-
-    // JavaScript form validation
-
-    var checkPassword = function(str)
-    {
-      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-      return re.test(str);
-    };
-
-    var checkForm = function(e)
-    {
-     re = /^\w+$/;
-     if(this.password1.value != "" && this.password1.value == this.password2.value) {
-        if(!checkPassword(this.pwd1.value)) {
-          alert("The password you have entered is not valid!");
-          this.password1.focus();
-          e.preventDefault();
-          return;
-        }
-      } else {
-        alert("Error: Please check that you've entered and confirmed your password!");
-        this.password1.focus();
-        e.preventDefault();
-        return;
-      }
-      alert("Both username and password are VALID!");
-    };
-
-    var myForm = document.getElementById("myForm");
-    myForm.addEventListener("submit", checkForm, true);
-
-    var supports_input_validity = function()
-    {
-      var i = document.createElement("input");
-      return "setCustomValidity" in i;
-    }
-
-      var pwd1Input = document.getElementById("password1");
-      pwd1Input.setCustomValidity(pwd1Input.title);
-
-      var pwd2Input = document.getElementById("password2");
-
-      pwd1Input.addEventListener("keyup", function(e) {
-        this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
-        if(this.checkValidity()) {
-          pwd2Input.pattern = RegExp.escape(this.value);
-          pwd2Input.setCustomValidity(pwd2Input.title);
-        } else {
-          pwd2Input.pattern = this.pattern;
-          pwd2Input.setCustomValidity("");
-        }
-      }, false);
-
-      pwd2Input.addEventListener("keyup", function(e) {
-        this.setCustomValidity(this.validity.patternMismatch ? pwd2Input.title : "");
-      }, false);
-
-    }, false);
+  if ((password1.value == password2.value) && password1.value.length > 7) {
+    button.type = "submit";
+    password1.style.color = 'green';
+    password2.style.color = 'green';
+    message.innerHTML = 'Password matching';
+    message.style.color = 'green';
+  }
+  else if(password1.value.length > 7) {
+    button.type = "button";
+    password1.style.color = 'red';
+    password2.style.color = 'red';
+    message.innerHTML = 'Password not matching';
+    message.style.color = 'red';
+  }
+  else{
+    button.type = "button";
+    password1.style.color = 'red';
+    password2.style.color = 'red';
+    message.innerHTML = 'Password must be at least 8 characters';
+    message.style.color = 'red';
+  }
+}
