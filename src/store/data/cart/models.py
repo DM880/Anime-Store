e29cf26c4851155cf8390829db0c197c5c19f43b
DM880 <denys.mauro@gmail.com>
2021-11-04 05:58:13 +0000

    cart models update

--- src/store/data/cart/models.py
+++ src/store/data/cart/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.datetime_safe import datetime
 
 
 from store.data.item.models import Item
@@ -6,5 +9,24 @@ from store.data.user.models import CustomUser as User
 
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    tot_count = models.IntegerField(default=0)
+    tot_price = models.IntegerField(default=0)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} have {} items, total price {}".format(self.user, self.tot_count, self.tot_price)
+
+
+class EntryCart(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, blank=True, null=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+
+
+@receiver(post_save, sender=EntryCart)
+def update_cart(sender, instance, **kwargs):
+    tot_item_cost = instance.quantity * instance.item.price
+    instance.cart.tot_price += tot_item_cost
+    instance.cart.tot_count += instance.quantity
+    instance.cart.updated = datetime.now()
