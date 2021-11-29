cce7fa4ae2dadd2ac85076c0310d7a11c3149698
DM880 <denys.mauro@gmail.com>
2021-11-29 05:26:09 +0000

    checkout js and guest added provvisory

--- src/store/interfaces/appsite/static/js/checkout_page.js
+++ src/store/interfaces/appsite/static/js/checkout_page.js
@@ -2,7 +2,7 @@ function decrementValue(item_id)
 {
     var id = item_id
     var value = parseInt(document.getElementById('quantity'+id).value);
-    if(value > 0){
+    if(value > 1){
     value = isNaN(value) ? 0 : value;
     value--;
     document.getElementById('quantity'+id).value = value;
